88375b0f599562542efd01794052189a32418828
anitha-anii <anithachowdary355@gmail.com>
2023-07-12 09:37:28 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-
-import React from "react";
-import './../styles/App.css';
+import React from 'react';
+import Tabs from './Tabs';
 
 const App = () => {
+  const tabs = [
+          {title: 'Tab 1'},
+           {title: 'Tab 2'},
+          {title: 'Tab 3'},
+  ];
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Tabs tabs={tabs} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
